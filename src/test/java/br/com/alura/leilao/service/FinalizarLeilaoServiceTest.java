a42523088db45a386217ecf11d8a3324cbae0ba3
Ademir Junior <ademiralcojr@gmail.com>
2022-09-04 13:30:45 -0300

    First commit

---
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,24 @@
+package br.com.alura.leilao.service;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class FinalizarLeilaoServiceTest {
+
+	private FinalizarLeilaoService service;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void deveriaFinalizarUmLeilao() {
+		
+	}
+
+}
